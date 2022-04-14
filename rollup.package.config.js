4cf700166d238743a2b85cdd7b089b0dc602e920
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-04-14 22:42:23 +0100

    1.0.0 - Clean up generated comments using rollup-plugin-cleanup

--- rollup.package.config.js
+++ rollup.package.config.js
@@ -1,12 +1,14 @@
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import cleanup from 'rollup-plugin-cleanup';
 
 export default [
     {
         input: 'src/main/cookieManager.ts',
         output: [{ file: './dist/index.js', format: 'esm' }],
         plugins: [
-            typescript({ tsconfig: './tsconfig.json' })
+            typescript({ tsconfig: './tsconfig.json' }),
+            cleanup({ comments: ['jsdoc'] })
         ]
     },
     {
