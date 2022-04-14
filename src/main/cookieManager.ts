25fea58cae1f4ac539fbded5a982c611cf7c0797
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-04-14 22:23:04 +0100

    1.0.0 - Added JSDoc comments to exposed functions

--- src/main/cookieManager.ts
+++ src/main/cookieManager.ts
@@ -7,32 +7,34 @@ import PreferencesFormHandler from './handlers/preferencesFormHandler';
 import { EventProcessor } from './handlers/eventHandler';
 import { CookieManagerConfig } from './interfaces/Config';
 
-const cookieManager = {
-    init: function (providedConfig: CookieManagerConfig): void {
-        console.debug('CookieManager initializing...');
+/**
+ * Initializes the @hmcts-cookie/manager library using the provided config.
+ *
+ * @param {CookieManagerConfig} providedConfig - Config for the library to use.
+ */
+function init (providedConfig: CookieManagerConfig): void {
+    console.debug('CookieManager initializing...');
 
-        const config = new Config(providedConfig);
-        const manifestHandler = new ManifestHandler(config);
-        const userPreferences = new UserPreferences(config, manifestHandler);
-        const cookieHandler = new CookieHandler(config, manifestHandler, userPreferences);
+    const config = new Config(providedConfig);
+    const manifestHandler = new ManifestHandler(config);
+    const userPreferences = new UserPreferences(config, manifestHandler);
+    const cookieHandler = new CookieHandler(config, manifestHandler, userPreferences);
 
-        userPreferences.processPreferences();
+    userPreferences.processPreferences();
 
-        if (Object.keys(config.getCookieBannerConfiguration()).length) {
-            new CookieBannerHandler(config, userPreferences, cookieHandler).init();
-        }
+    if (Object.keys(config.getCookieBannerConfiguration()).length) {
+        new CookieBannerHandler(config, userPreferences, cookieHandler).init();
+    }
 
-        if (Object.keys(config.getPreferencesFormConfiguration()).length) {
-            new PreferencesFormHandler(config, userPreferences, cookieHandler).init();
-        }
+    if (Object.keys(config.getPreferencesFormConfiguration()).length) {
+        new PreferencesFormHandler(config, userPreferences, cookieHandler).init();
+    }
 
-        EventProcessor.emit('CookieManagerLoaded');
-
-        cookieHandler.processCookies();
-    },
-    on: EventProcessor.on,
-    off: EventProcessor.off
+    EventProcessor.emit('CookieManagerLoaded');
+    cookieHandler.processCookies();
 };
 
-export default cookieManager;
-export type { CookieManagerConfig };
+const on = EventProcessor.on;
+const off = EventProcessor.off;
+
+export default { on, off, init };
